579e209dc07c002de0d0fd8e916d0e41c26dbe7d
AnhP <112260892+AnhP1205@users.noreply.github.com>
2026-02-21 14:05:00 -0800

    I have already create a time object, and make it 3 second delay before it first fire

--- src/MyFirstTimer.java
+++ src/MyFirstTimer.java
@@ -1,23 +1,44 @@
 import acm.graphics.*;
 import acm.program.*;
+import javax.swing.*;
+import java.awt.event.*;
 
-public class MyFirstTimer extends GraphicsProgram {
-	public static final int PROGRAM_HEIGHT = 600;
-	public static final int PROGRAM_WIDTH = 800;
-	public static final int MAX_STEPS = 20;
-	private GLabel myLabel;
+public class MyFirstTimer extends GraphicsProgram implements ActionListener {
+    public static final int PROGRAM_HEIGHT = 600;
+    public static final int PROGRAM_WIDTH = 800;
+    public static final int MAX_STEPS = 20;
 
-	public void init() {
-		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
-		requestFocus();
-	}
-	
-	public void run() {
-		myLabel = new GLabel("# of times called?", 0, 100);
-		add(myLabel);
-	}
-	
-	public static void main(String[] args) {
-		new MyFirstTimer().start();
-	}
+    private GLabel myLabel;
+    private int numTimes;
+    private Timer t;
+
+    public void init() {
+        setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
+        requestFocus();
+    }
+
+    public void run() {
+        myLabel = new GLabel("times called? 0", 0, 100);
+        add(myLabel);
+
+        numTimes = 0;
+
+        t = new Timer(1000, this);
+        t.setInitialDelay(3000); 
+        t.start();
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        myLabel.move(5, 0);
+        numTimes++;
+        myLabel.setLabel("times called? " + numTimes);
+
+        if (numTimes == 10) {
+            t.stop();
+        }
+    }
+
+    public static void main(String[] args) {
+        new MyFirstTimer().start();
+    }
 }
